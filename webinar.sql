e2e4af67cce30a777047aa2475862ef83044bd25
cha044 <124748855+cha044@users.noreply.github.com>
2024-05-23 07:49:32 +0400

    Add files via upload
    sql file

---
+++ webinar.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE php_task;
+
+USE php_task;
+
+CREATE TABLE webinars (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    event_id VARCHAR(255),
+    start_time DATETIME NOT NULL,
+    end_time DATETIME NOT NULL,
+    webinar_key VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    source VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
